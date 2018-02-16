ae6ca7d53607c9bbd59278b146fc54aa63275283
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-16 14:00:21 +1100

    feat: add Heading component

---
+++ client/components/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  color: blue;
+  font-size: 25px;
+  font-weight: bold;
+  padding: 0 20px 0 20px;
+`;
+
+export default Heading
