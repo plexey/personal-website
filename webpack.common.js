3920b012a23068161c82857dd9cc8295de87a7d9
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-17 10:21:22 +1100

    refactor: split webpack.config.js into common, dev, prod

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './client/index.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Producton',
+      template: './client/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ],
+  output: {
+    filename: 'index_bundle.js',
+    path: path.resolve('dist')
+  }
+}
+
