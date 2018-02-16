765e0bd8a8945ed28333ca0106f9df3f8727dd32
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-16 15:54:05 +1100

    refactor: add font-family via prop

--- client/components/Heading.js
+++ client/components/Heading.js
@@ -2,10 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Heading = styled.h1`
-  color: blue;
+  color: hsl(0, 0%, 85%);
   font-size: 25px;
   font-weight: bold;
   padding: 0 20px 0 20px;
+  font-family: ${props => props.theme.font}
 `;
 
 export default Heading
