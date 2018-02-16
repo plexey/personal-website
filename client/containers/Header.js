39c687f7820a5a57a26ef12edb9ee7cb2f17d137
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-16 16:01:16 +1100

    feat: add Heading component feat: add Navlinks

--- client/containers/Header.js
+++ client/containers/Header.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import Heading from '../components/Heading';
+import NavLink from '../components/NavLink';
 
-// Define our button, but with the use of props.theme this time
-const Header = styled.header`
+const HeaderContainer = styled.header`
   display: flex;
   flex-direction: row;
+  align-content: center;
+  align-items: center;
   height: 70px;
-  background: hsla(0, 50%, 50%, 1);
+  background: hsl(220, 16%, 22%);
 `;
 
-export default Header;
+export default class Header extends React.Component {
+  render() {
+    return (
+      <HeaderContainer>
+        <Heading>Heading</Heading>
+        <NavLink exact to="/">Home</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </HeaderContainer>
+    );
+  }
+}
