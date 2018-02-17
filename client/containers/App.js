2942a7b05ecf5393d2ced0f515a49703f8ad676c
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-17 23:06:02 +1100

    refactor: define css grid columns & rows on Main component
    feat: add ContentHeader component
    
    feat: add Footer component

--- client/containers/App.js
+++ client/containers/App.js
@@ -2,22 +2,33 @@ import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import baseStyles from '../baseStyles';
 import Header from './Header';
+import ContentHeader from './ContentHeader';
 import Content from './Content';
+import Footer from './Footer';
 
 const theme = {
+  brandColor: 'hsl(130, 80%, 70%)',
   font: `'Oxygen', sans-serif`,
 }
+
+const Main = styled.div`
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: [main1] 60px [main2] 190px [main3] auto [main4] 60px [main5];
+`
 
 export default class App extends React.Component {
   render() {
     baseStyles()
     return (
-        <ThemeProvider theme={theme}>
-          <div>
-            <Header />
-            <Content />
-          </div>
-        </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Main>
+          <Header />
+          <ContentHeader />
+          <Content />
+          <Footer />
+        </Main>
+      </ThemeProvider>
     );
   }
 }
