981ad845100b718470e5667f2623fc15ac928a26
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-17 23:03:05 +1100

    style: tweak component color, padding

--- client/components/Heading.js
+++ client/components/Heading.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Heading = styled.h1`
-  color: hsl(0, 0%, 85%);
+  color: hsl(0, 0%, 10%);
   font-size: 25px;
   font-weight: bold;
-  padding: 0 20px 0 20px;
-  font-family: ${props => props.theme.font}
+  padding: 0 30px 0 0;
+  font-family: ${props => props.theme.font};
 `;
 
 export default Heading
