52ecf3d47bedd51e4518e559d9006aa32fb0ee3b
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-17 23:01:54 +1100

    feat: add content containing sample data used throughout app

---
+++ client/content.js
@@ -0,0 +1,54 @@
+const content = {
+
+  palettetown: {
+    heading: "Palette Town",
+    subheading: "Subheading text",
+    description: "A color generation utility",
+    features: [
+      "feature one",
+      "feature two",
+      "feature three",
+      "feature four"
+    ],
+    links: {
+      github: "https://github.com/plexey/palette-town",
+      demo: "http://palettetown.surge.sh/"
+    }
+  },
+
+  reactdepthoffield: {
+    heading: "React Depth of Field",
+    subheading: "Subheading text",
+    description: "A React module for applying a dynamic depth of field effect to a set of elements.",
+    features: [
+      "feature one",
+      "feature two",
+      "feature three",
+      "feature four"
+    ],
+    links: {
+      github: "https://github.com/plexey/react-depth-of-field",
+      npm: "https://www.npmjs.com/package/react-depth-of-field",
+      demo: "http://react-depth-of-field.surge.sh/"
+    }
+  },
+
+  reactreduxtodo: {
+    heading: "React Redux Todo",
+    subheading: "A todo app built with React & Redux",
+    description: " ",
+    features: [
+      "feature one",
+      "feature two",
+      "feature three",
+      "feature four"
+    ],
+    links: {
+      github: "https://github.com/plexey/react-redux-todo",
+      demo: "http://reactreduxtodo.surge.sh/"
+    }
+  }
+  
+}
+
+export default content 
