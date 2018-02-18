7121fa90abf0803078eb1812d7ea82f2b0e41d78
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-18 18:35:06 +1100

    refactor: define 'mainGridTemplateColums' containing grid column layout for app

--- client/containers/App.js
+++ client/containers/App.js
@@ -7,14 +7,16 @@ import Content from './Content';
 import Footer from './Footer';
 
 const theme = {
-  brandColor: 'hsl(130, 80%, 70%)',
+  brandColor: 'hsl(170, 100%, 60%)',
+  headingFont: `'Walter Turncoat', cursive`,
   font: `'Oxygen', sans-serif`,
+  mainGridTemplateColums: '[cline-1] 15% [cline-2] auto [cline-3] 15% [cline-4]'
 }
 
 const Main = styled.div`
   display: grid;
   grid-template-columns: 100%;
-  grid-template-rows: [main1] 60px [main2] 190px [main3] auto [main4] 60px [main5];
+  grid-template-rows: [main1] 60px [main2] 190px [main3] auto [main4] 80px [main5];
 `
 
 export default class App extends React.Component {
