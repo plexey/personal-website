3d869423aaa543b35cd948213a862a007aef19b9
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-18 18:38:02 +1100

    refactor: add FooterContainer, FooterContent and Heading components and update layout

--- client/containers/Footer.js
+++ client/containers/Footer.js
@@ -1,9 +1,39 @@
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const Footer = styled.footer`
+const FooterContainer = styled.footer`
+  display: grid;
   grid-row-start: main4;
   grid-row-end: main5;
-  background: yellow;
+  /* Define internal grid columns */
+  grid-template-columns: ${props => props.theme.mainGridTemplateColums};
+  background: hsl(0, 0%, 80%);
+  font-family: ${props => props.theme.font};
 `;
+
+const FooterContent = styled.div`
+  grid-column-start: cline-2;
+  grid-column-end: cline-3;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Heading = styled.h1`
+  color: hsl(0, 0%, 50%);
+  font-weight: bold;
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <Heading>Copyright &copy; 2018 - Thomas Trinca</Heading>
+      </FooterContent>
+    </FooterContainer>
+  );
+};
 
 export default Footer;
