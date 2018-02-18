301ec555258cc7f0035bf8894c7f50a62b3e61cd
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-18 18:34:12 +1100

    refactor: add rules preventing text overflow

--- client/components/Heading.js
+++ client/components/Heading.js
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 
 const Heading = styled.h1`
   color: hsl(0, 0%, 10%);
-  font-size: 25px;
+  font-size: 29px;
   font-weight: bold;
   padding: 0 30px 0 0;
-  font-family: ${props => props.theme.font};
+  font-family: ${props => props.theme.headingFont};
+  text-decoration: underline;
+  white-space: nowrap;
 `;
 
 export default Heading
