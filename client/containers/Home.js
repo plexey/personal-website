6d205469a70d92469112026f01826e8b428eb058
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-18 23:33:46 +1100

    feat: add Home component

---
+++ client/containers/Home.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  grid-column-start: cline-2;
+  grid-column-end: cline-3;
+  font-family: ${props => props.theme.font};
+  padding: 0 0 60px 0;
+  display: grid;
+  /* grid-template-rows: auto;  */
+`;
+
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px 0 0 0;
+`;
+
+const Portrait = styled.img`
+  width: 200px;
+  border-radius: 50%;
+  border-style: solid;
+  border-color: hsl(0, 0%, 20%);
+  border-width: 4px;
+  margin: 20px;
+`;
+
+const Heading = styled.h2`
+  color: hsl(0, 0%, 15%);
+  font-size: 25px;
+  font-weight: bold;
+`;
+
+const Subheading = styled.h3`
+  color: hsl(0, 0%, 35%);
+  font-size: 20px;
+`;
+
+const Paragraph = styled.p`
+  font-size: 17px;
+  margin: 10px 0 0 0;
+`;
+
+// const Bio = styled.
+
+const Home = () => {
+  return (
+    <Wrapper>
+      <Content>
+        <Heading>Tom Trinca - Web Developer</Heading>
+        <Subheading>Melbourne, Australia</Subheading>
+        <Portrait src="https://i.imgur.com/SquGERT.jpg" alt="mugshot" />
+        <Paragraph>
+          I am a Web Developer based in Melbourne, Australia. I have a varied
+          background with a Diploma of Website Development, a Bachelor’s degree
+          in both General Arts & Visual Arts as well as a Masters degree in Arts
+          Management (with Distinction). These qualifications have served to
+          provide me with diverse skill set including but not limited to
+          programming, design and project management.
+        </Paragraph>
+        <Paragraph>
+          I have worked on a multitude of web projects, designing and building
+          responsive and elegant user interfaces and utilities using cutting
+          edge technologies. During my Visual Arts degree I acquired a keen eye
+          for architecture, and design.
+        </Paragraph>
+        <Paragraph>
+          I am proficient with the following languages: JavaScript, HTML, CSS,
+          Java, Python, C#, and SQL. I am also proficient with the following
+          frameworks: React, Vue, and ASP.NET. I have extensive experience with
+          the following state management tools: Redux and Mobx
+        </Paragraph>
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Home;
