1b08cf40dae3ceffe036c76d194c96413cf93e16
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-19 17:44:12 +1100

    refactor: add ImageGallery component to support image gallery
    style: format with Prettier

--- client/containers/Projects.js
+++ client/containers/Projects.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import content from '../content';
-import Section from '../components/Section';
-import Paragraph from '../components/Paragraph';
-import SectionHeading from '../components/SectionHeading';
+import React, { Component } from "react";
+import styled from "styled-components";
+import ImageGallery from "react-image-gallery";
+import content from "../content";
+import Section from "../components/Section";
+import SectionContent from "../components/SectionContent";
+import Paragraph from "../components/Paragraph";
+import SectionHeading from "../components/SectionHeading";
+import SectionSubeading from "../components/SectionSubeading";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 const Wrapper = styled.div`
   grid-column-start: cline-2;
@@ -12,32 +16,43 @@ const Wrapper = styled.div`
   padding: 0 0 60px 0;
 `;
 
+const GalleryWrapper = styled.div`
+  margin: 40px 0 0 0;
+  box-shadow: 0 0 20px 0 hsla(0, 0%, 0%, 0.1);
+`;
+
 const ListItems = styled.ul`
   font-size: 19px;
 `;
 
 const ListItem = styled.li`
   padding: 5px 0 5px 0;
+  margin: 0 0 0 20px;
+  list-style-type: circle;
 `;
 
 const Link = styled.a`
   font-weight: bold;
   text-decoration: none;
-  color: hsl(220, 50%, 50%);
-`
+  color: hsl(220, 0%, 20%);
+  padding: 5px 5px 5px 5px;
+  border-radius: 3px;
+
+  ${Link}:hover {
+    background: ${props => props.theme.brandColor};
+  }
+`;
 
 const Image = styled.img`
   margin: 50px 0 20px 0;
-  width: 70%;
-  min-width: 800px;
-  max-width: 1000px;
+  width: 100%;
 
   @media screen and (max-width: 1000px) {
     ${Image} {
-        min-width: 100%;
-        width: 100%;
+      min-width: 100%;
+      width: 100%;
     }
-}
+  }
 `;
 
 class Projects extends React.Component {
@@ -46,54 +61,87 @@ class Projects extends React.Component {
     const projData = content[match.params.project];
     return (
       <Wrapper>
-
         {/* Image */}
 
-        {projData.image !== undefined &&
+        {projData.images !== undefined && (
+          <GalleryWrapper>
+            <ImageGallery
+              showThumbnails={false}
+              showBullets={true}
+              items={projData.images}
+              showFullscreenButton={false}
+              showPlayButton={false}
+            />
+          </GalleryWrapper>
+        )}
+
+        {/* {projData.image !== undefined && (
           <Image src={projData.image} alt={projData.heading} />
-        }
+        )} */}
 
         <Section>
-        <SectionHeading>Description</SectionHeading>
-        <Paragraph>
-          {projData.description}
-        </Paragraph>
+          <SectionHeading>About this project</SectionHeading>
+          <SectionContent>
+            <Paragraph>{projData.description}</Paragraph>
+          </SectionContent>
         </Section>
 
         {/* Features */}
 
-        {projData.features !== undefined &&
+        {projData.features !== undefined && (
           <Section>
             <SectionHeading>Features</SectionHeading>
-            <ListItems>
-              {projData.features.map((item, index) =>
-                <ListItem key={item + "-" + index}>
-                  {item}
-                </ListItem>
-              )}
-            </ListItems>
+            <SectionContent>
+              <ListItems>
+                {projData.features.map((item, index) => (
+                  <ListItem key={item + "- " + index}>{item}</ListItem>
+                ))}
+              </ListItems>
+            </SectionContent>
           </Section>
-        }
+        )}
 
         {/* Technologies */}
 
-        {projData.technologies !== undefined &&
+        {projData.technologies !== undefined && (
           <Section>
-            <SectionHeading>Technologies</SectionHeading>
-            <ListItems>
-              {projData.technologies.map((item, index) =>
-                <ListItem key={item.name + "-" + index}>
-                  <Link href={item.link}>{item.name}</Link> - {item.description}
-                </ListItem>
-              )}
-            </ListItems>
+            <SectionHeading>Technical Sheet</SectionHeading>
+            <SectionSubeading>
+              Code technologies used while working on this project
+            </SectionSubeading>
+            <SectionContent>
+              <ListItems>
+                {projData.technologies.map((item, index) => (
+                  <ListItem key={item.name + "-" + index}>
+                    <Link href={item.link}>{item.name}</Link> -{" "}
+                    {item.description}
+                  </ListItem>
+                ))}
+              </ListItems>
+            </SectionContent>
           </Section>
-        }
+        )}
 
+        {/* Resources */}
+
+        {projData.resources !== undefined && (
+          <Section>
+            <SectionHeading>Resources</SectionHeading>
+            <SectionContent>
+              <ListItems>
+                {projData.resources.map((item, index) => (
+                  <ListItem key={item.name + "-" + index}>
+                    {item.name}
+                    <Link href={item.link}>{item.alias}</Link>
+                  </ListItem>
+                ))}
+              </ListItems>
+            </SectionContent>
+          </Section>
+        )}
       </Wrapper>
     );
   }
 }
-
 
 export default Projects;
