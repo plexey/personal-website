8ca68ce2a2f58a98db64ea81e579aeaed389d709
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-19 17:41:29 +1100

    refactor: add media query to support tablets
    style: format with Prettier
    
    style: add css rules to styled Link component

--- client/containers/ContentHeader.js
+++ client/containers/ContentHeader.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Route, NavLink } from 'react-router-dom';
-import content from '../content';
-
+import React from "react";
+import styled from "styled-components";
+import { Route } from "react-router-dom";
+import content from "../content";
 
 const Wrapper = styled.div`
   display: grid;
@@ -13,6 +12,12 @@ const Wrapper = styled.div`
   background: hsl(0, 0%, 20%);
   font-family: ${props => props.theme.font};
 
+  @media screen and (max-width: 1300px) {
+    ${Wrapper} {
+      grid-template-columns: ${props => props.theme.tabGridTempCol};
+    }
+  }
+
   @media screen and (max-width: 1000px) {
     ${Wrapper} {
       grid-template-columns: ${props => props.theme.phoneGridTempCol};
@@ -20,24 +25,29 @@ const Wrapper = styled.div`
   }
 `;
 
+const NavArrow = styled.div`
+  color: white;
+  font-size: 30px;
+`;
+
 const Content = styled.div`
   display: grid;
   grid-column-start: cline-2;
   grid-column-end: cline-3;
   /* Define internal grid rows */
-  grid-template-rows: 
-    [ch-row-1] 
-      10px 
-    [ch-row-2] 
-      auto 
-    [ch-row-3] 
-      auto 
-    [ch-row-4] 
-      auto 
-    [ch-row-5] 
-      20px 
+  grid-template-rows:
+    [ch-row-1]
+    10px
+    [ch-row-2]
+    auto
+    [ch-row-3]
+    auto
+    [ch-row-4]
+    auto
+    [ch-row-5]
+    20px
     [ch-row-6];
-`
+`;
 
 const Heading = styled.h2`
   grid-row-start: ch-row-2;
@@ -47,7 +57,7 @@ const Heading = styled.h2`
   align-items: flex-end;
   font-size: 35px;
   font-weight: bold;
-`
+`;
 
 const Subheading = styled.h3`
   grid-row-start: ch-row-3;
@@ -57,23 +67,33 @@ const Subheading = styled.h3`
   align-items: center;
   font-size: 20px;
   color: hsl(0, 0%, 90%);
-`
+`;
 
 const NavLinks = styled.nav`
-    grid-row-start: ch-row-4;
-    grid-row-end: ch-row-5;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-`
+  grid-row-start: ch-row-4;
+  grid-row-end: ch-row-5;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
 
 const Link = styled.a`
-  padding: 0 20px 0 0;
-  color: ${props => props.theme.brandColor};
+  padding: 10px 20px 10px 20px;
+  margin: 0 10px 0 0;
+  border-radius: 2px;
+  background: ${props => props.theme.brandColor};
   font-size: 18px;
-`
+  color: hsl(0, 0%, 20%);
+  text-decoration: none;
+  font-weight: bold;
+  transition: 200ms ease all;
 
-const ProjectContentHeader = (props) => {
+  ${Link}:hover {
+    transform: scale(1.05, 1.05);
+  }
+`;
+
+const ProjectContentHeader = props => {
   const projData = content[props.match.params.project];
   const linkNames = Object.keys(projData.links);
   const linkPaths = Object.values(projData.links);
@@ -82,35 +102,42 @@ const ProjectContentHeader = (props) => {
       <Heading>{projData.heading}</Heading>
       <Subheading>{projData.subheading}</Subheading>
       <NavLinks>
-        {linkNames.map((item, index) =>
-          <Link key={index} href={linkPaths[index]}>{item}</Link>
-        )}
+        {linkNames.map((item, index) => (
+          <Link key={index} href={linkPaths[index]}>
+            {item}
+          </Link>
+        ))}
       </NavLinks>
     </Content>
-  )
-}
+  );
+};
 
 const ContentHeader = () => (
   <Wrapper>
-
-    <Route exact path="/" render={() => (
-      <Content>
-        <Heading>Home</Heading>
-        <Subheading>Some info about me</Subheading>
-      </Content>
-    )} />
+    <Route
+      exact
+      path="/"
+      render={() => (
+        <Content>
+          <Heading>Home</Heading>
+          <Subheading>Some info about me</Subheading>
+        </Content>
+      )}
+    />
 
     <Route exact path="/projects/:project" component={ProjectContentHeader} />
 
-    <Route exact path="/Contact" render={() => (
-      <Content>
-        <Heading>Contact</Heading>
-        <Subheading>Get in touch :)</Subheading>
-      </Content>
-    )} />
-
+    <Route
+      exact
+      path="/Contact"
+      render={() => (
+        <Content>
+          <Heading>Contact</Heading>
+          <Subheading>Get in touch :)</Subheading>
+        </Content>
+      )}
+    />
   </Wrapper>
-)
-
+);
 
 export default ContentHeader;
