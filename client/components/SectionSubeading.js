5c5daa9f3f1760bcd99ee4ecfcc9c606afe20b94
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-19 17:39:16 +1100

    feat: add SectonSubHeading styled component

---
+++ client/components/SectionSubeading.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const SectionHeading = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+  width: 100%;
+  color: hsl(0, 0%, 40%);
+  margin: 15px 0 0 0;
+`;
+
+export default SectionHeading
