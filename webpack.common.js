21e828db8d3276e467996945ea5524c338ab6192
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-19 17:36:32 +1100

    refactor: add CSS support via style-loader and css-loader added to module loaders

--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,34 @@
-const path = require('path');
+const path = require("path");
 
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: './client/index.js'
+    app: "./client/index.js"
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader']}
     ]
   },
   devServer: {
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
-      title: 'Producton',
-      template: './client/index.html',
-      filename: 'index.html',
-      inject: 'body'
+      title: "Producton",
+      template: "./client/index.html",
+      filename: "index.html",
+      inject: "body"
     })
   ],
   output: {
-    filename: 'index_bundle.js',
-    path: path.resolve('dist'),
-    publicPath: '/'
+    filename: "index_bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/"
   }
-}
-
+};
