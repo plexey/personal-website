d5c53db7611475bf1eaa8ca62dd6ba11d662f624
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-19 17:38:04 +1100

    style: tweak box style

--- client/components/Section.js
+++ client/components/Section.js
@@ -5,6 +5,8 @@ const Section = styled.div`
   background: hsl(0, 0%, 95%);
   padding: 20px;
   margin: 40px 0 0 0;
+  border-radius: 3px;
+  box-shadow: 0 0 20px 0 hsla(0, 0%, 0%, 0.1);
   
   @media screen and (max-width: 1000px) {
     ${Section} {
