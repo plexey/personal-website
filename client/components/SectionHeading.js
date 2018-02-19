51fc4301a242413e51452c1140df33698b3f14b4
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-19 17:38:57 +1100

    style: tweak styling

--- client/components/SectionHeading.js
+++ client/components/SectionHeading.js
@@ -3,14 +3,10 @@ import styled from 'styled-components';
 
 const SectionHeading = styled.h1`
   font-size: 27px;
-  padding: 0 0 10px 0;
-  margin: 0 0 15px 0;
+  /* margin: 0 0 15px 0; */
   font-weight: bold;
   width: 100%;
-  border-style: solid;
-  border-width: 0 0 1px 0;
-  border-color: hsla(0, 0%, 0%, .15);
-  color: hsl(0, 0%, 15%);
+  color: hsl(0, 0%, 20%);
 `;
 
 export default SectionHeading
