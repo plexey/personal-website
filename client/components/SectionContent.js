8bafc29730c02a8832b9e7a3c9facee45c9cf916
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-19 17:38:34 +1100

    feat: add SectionContent styled component

---
+++ client/components/SectionContent.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const SectionContent = styled.div`
+  margin: 20px 0 0 0;
+  padding: 20px 0 0 0;
+  border-style: solid;
+  border-width: 1px 0 0 0;
+  border-color: hsla(0, 0%, 0%, 0.15);
+`;
+
+export default SectionContent;
