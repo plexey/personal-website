653cc0ed5eafd9b4acbdfd76ce5d5a5db1e2b1c2
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-20 23:05:32 +1100

    refactor: use textColor prop as color from ThemeProvider

--- client/components/SectionHeading.js
+++ client/components/SectionHeading.js
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 
 const SectionHeading = styled.h1`
   font-size: 27px;
-  /* margin: 0 0 15px 0; */
   font-weight: bold;
   width: 100%;
-  color: hsl(0, 0%, 20%);
+  color: ${props => props.theme.textColor};
 `;
 
 export default SectionHeading
