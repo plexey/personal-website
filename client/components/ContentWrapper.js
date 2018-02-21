902d97bb7aaef128f2b94e4c2a59d9bf61088539
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-21 23:29:06 +1100

    refactor: remove unused imports refactor: improve support for mobile screens

--- client/components/ContentWrapper.js
+++ client/components/ContentWrapper.js
@@ -1,10 +1,16 @@
-import React from "react";
 import styled from "styled-components";
 
 const ContentWrapper = styled.div`
   grid-column-start: cline-2;
   grid-column-end: cline-3;
   font-family: ${props => props.theme.font};
+  margin: 30px 0 30px 0;
+
+  @media screen and (max-width: 1000px) {
+    ${ContentWrapper} {
+      margin: 15px 0 15px 0;
+    }
+  }
 `;
 
 export default ContentWrapper;
