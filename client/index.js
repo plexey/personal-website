44c8871aa6eb88db820700e63ad864369ad2111f
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-21 23:27:59 +1100

    style: format with Prettier refactor: remove unused imports

--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App.js';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
