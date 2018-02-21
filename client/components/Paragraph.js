7130e02f1b1c394b449ff14d58075d8289d77bba
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-21 23:30:15 +1100

    refactor: remove unused imports refactor: improve support for mobile screens

--- client/components/Paragraph.js
+++ client/components/Paragraph.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Paragraph = styled.p`
   font-size: 20px;
+  margin: 0 0 10px 0;
+
+  @media screen and (max-width: 1000px) {
+    ${Paragraph} {
+      font-size: 16px;
+    }
+  }
 `;
 
 export default Paragraph;
