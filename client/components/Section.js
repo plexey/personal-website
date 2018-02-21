2021800134533cdb6b06e60cb91f43fa9623a37d
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-21 23:30:46 +1100

    refactor: remove unused imports style: remove commented out css

--- client/components/Section.js
+++ client/components/Section.js
@@ -1,16 +1,11 @@
-import React from 'react';
 import styled from 'styled-components';
 
 const Section = styled.div`
-  /* background: hsl(0, 0%, 95%); */
-  padding: 20px;
   margin: 40px 0 0 0;
-  /* border-radius: 3px; */
-  /* box-shadow: 0 0 20px 0 hsla(0, 0%, 0%, 0.1); */
   
   @media screen and (max-width: 1000px) {
     ${Section} {
-      margin: 20px 0 0 0;
+      margin: 20px 0 40px 0;
     }
   }
 `
