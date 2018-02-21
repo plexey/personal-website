782a31dfec7ee49d7e6a467e1ef4db08c1157120
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-21 23:41:29 +1100

    refactor: remove unused imports style: format with Prettier

--- client/containers/Home.js
+++ client/containers/Home.js
@@ -1,23 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
-
-const Wrapper = styled.div`
-  grid-column-start: cline-2;
-  grid-column-end: cline-3;
-  font-family: ${props => props.theme.font};
-`;
-
-const Content = styled.div`
-  padding: 20px;
-  max-width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 70px 0 0 0;
-  background: hsl(0, 0%, 95%);
-  border-radius: 3px;
-  box-shadow: 0 0 20px 0 hsla(0, 0%, 0%, 0.1);
-`;
+import Paragraph from '../components/Paragraph';
+import ContentWrapper from "../components/ContentWrapper";
 
 const Portrait = styled.img`
   width: 200px;
@@ -39,42 +23,33 @@ const Subheading = styled.h3`
   font-size: 20px;
 `;
 
-const Paragraph = styled.p`
-  font-size: 19px;
-  margin: 10px 0 0 0;
-`;
-
-// const Bio = styled.
-
 const Home = () => {
   return (
-    <Wrapper>
-      <Content>
-        <Heading>Tom Trinca - Web Developer</Heading>
-        <Subheading>Melbourne, Australia</Subheading>
-        <Portrait src="https://i.imgur.com/SquGERT.jpg" alt="mugshot" />
-        <Paragraph>
-          I am a Web Developer based in Melbourne, Australia. I have a varied
-          background with a Diploma of Website Development, a Bachelor’s degree
-          in General Arts & Visual Arts as well as a Masters degree in Arts
-          Management (with Distinction). These qualifications have provided me
-          with diverse skill set including but not limited to programming,
-          design and project management.
-        </Paragraph>
-        <Paragraph>
-          I have worked on a multitude of web projects, designing and building
-          responsive and elegant user interfaces and utilities using cutting
-          edge technologies. During my Visual Arts degree I acquired a keen eye
-          for architecture, and design.
-        </Paragraph>
-        <Paragraph>
-          I am proficient with the following languages: JavaScript, HTML, CSS,
-          Java, Python, C#, and SQL. I am also proficient with the following
-          frameworks: React, Vue, and ASP.NET. I have extensive experience with
-          the following state management tools: Redux and Mobx
-        </Paragraph>
-      </Content>
-    </Wrapper>
+    <ContentWrapper>
+      <Heading>Tom Trinca - Web Developer</Heading>
+      <Subheading>Melbourne, Australia</Subheading>
+      <Portrait src="https://i.imgur.com/SquGERT.jpg" alt="mugshot" />
+      <Paragraph>
+        I am a Web Developer based in Melbourne, Australia. I have a varied
+        background with a Diploma of Website Development, a Bachelor’s degree in
+        General Arts & Visual Arts as well as a Masters degree in Arts
+        Management (with Distinction). These qualifications have provided me
+        with diverse skill set including but not limited to programming, design
+        and project management.
+      </Paragraph>
+      <Paragraph>
+        I have worked on a multitude of web projects, designing and building
+        responsive and elegant user interfaces and utilities using cutting edge
+        technologies. During my Visual Arts degree I acquired a keen eye for
+        architecture, and design.
+      </Paragraph>
+      <Paragraph>
+        I am proficient with the following languages: JavaScript, HTML, CSS,
+        Java, Python, C#, and SQL. I am also proficient with the following
+        frameworks: React, Vue, and ASP.NET. I have extensive experience with
+        the following state management tools: Redux and Mobx
+      </Paragraph>
+    </ContentWrapper>
   );
 };
 
