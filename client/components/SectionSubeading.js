869ca96d9c756e5795770753104f0583a9a9db51
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-21 23:31:18 +1100

    refactor: remove unused imports refactor: improve support for mobile screens

--- client/components/SectionSubeading.js
+++ client/components/SectionSubeading.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const SectionHeading = styled.h1`
@@ -7,6 +6,12 @@ const SectionHeading = styled.h1`
   width: 100%;
   color: hsl(0, 0%, 40%);
   margin: 15px 0 0 0;
+
+    @media screen and (max-width: 1000px) {
+    ${SectionHeading} {
+      font-size: 18px;
+    }
+  }
 `;
 
 export default SectionHeading
