a0d39cf8cb1cc882cabe7e847aa5c7223e3b2d7c
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-24 16:23:42 +1100

    refactor: replace hard coded NavLink 'to' props with external variables style: format with Prettier

--- client/containers/Content.js
+++ client/containers/Content.js
@@ -12,8 +12,9 @@ const Wrapper = styled.div`
   grid-row-end: main4;
   grid-template-columns: ${props => props.theme.deskGridTempCol};
   min-height: 100vh;
-  background: hsl(0, 0%, 90%);
   transition: 200ms ease all;
+  background-color: #f4f4f4;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%23e0e0e0' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E");
 
   @media screen and (max-width: 1300px) {
     ${Wrapper} {
