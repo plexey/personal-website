e0eac5e49f68cca90b735d8e937b82a5559158af
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-24 16:17:26 +1100

    refactor: rename contnet to data

--- client/data.js
+++ client/data.js
@@ -4,12 +4,7 @@ const example = {
     heading: "PROJ_NAME",
     subheading: "PROJ_SUBHEADING",
     description: "PROJ_DESCRIPTION",
-    images: [
-      { original: "", thumnail: "" },
-      { original: "", thumnail: "" },
-      { original: "", thumnail: "" }
-    ],
-    features: ["FEATURE_NAME", "FEATURE_NAME"],
+    images: [{ original: "" }, { original: "" }, { original: "" }],
     technologies: [
       { name: "TECH_NAME", description: "TECH_DESC", link: "TECH_LINK" }
     ],
@@ -26,15 +21,18 @@ const example = {
   }
 };
 
-const content = {
+const projects = {
   palettetown: {
     name: "palettetown",
     heading: "Palette Town",
     subheading:
       "A sophisticated color palette generation utility for designers & developers",
-    description: `A sophisticated color palette generator and manipulation utility. Palette Town allows you to generate an infinite variety of color palettes from selected color harmonies and modifiers.Reorder color palettes arbitrarily by dragging and dropping swatches. Add and remove individual swatches from your palette or modify swatches color values in your preferred color space.`,
+    description: `Palette Town is a tool for creating and refining unique color palettes for your website or other project. With it, you can generate triadic, tetradic, square, pentagonal color harmonies or alternatively an entirely randomized color palette. 
+    Reorder a generated palette by dragging and dropping its swatches or by clicking the shuffle and reverse buttons. Modify a given swatch's color values within your preferred color space or, alternatively, add and remove swatches at will. 
+    With Palette Town you can save and name your palettes to the Favorites section and easily export its hex, rgb, hsl or cmyk color values to your project.`,
     images: [
       { original: "https://i.imgur.com/3iG9kU9.png" },
+      { original: "https://i.imgur.com/KnP4AzG.png" },
       { original: "https://i.imgur.com/134eGjO.png" },
       { original: "https://i.imgur.com/8B91few.png" }
     ],
@@ -108,19 +106,27 @@ const content = {
         link: "https://github.com/plexey/palette-town"
       }
     ],
-    tags: ["javascript", "react", "mobx", "css modules"]
+    tags: [
+      "javascript",
+      "react",
+      "mobx",
+      "css modules",
+      "server rendering",
+      "travis ci"
+    ]
   },
   reactdepthoffield: {
     name: "reactdepthoffield",
     heading: "React Depth of Field",
     subheading:
-      "A React module for applying a dynamic depth of field to a set of elements",
-    description:
-      "A React module for applying a dynamic depth of field effect to a set of elements.",
+      "A React module for applying a dynamic, animated depth of field effect to a set of elements",
+    description: `This module is a React component designed to apply an adjustable, animatable depth of field effect to a set of provided children. Amongst other props, it takes a set of children and a designated focus point. The focus point is the index of the child element to be in focus. 
+      From the designed focus point, the module applies a depth of field effect to its children centered on a given child by programatically applying CSS blur filters of increasing strengths to each of its children above and below the focus point. 
+      Changing the focus point to a different child index will initiate an animated transition from the orignal index to the new index, simulating a camera focus pull. The module also accepts a provided blur increment as well as an animation time prop. The former controls the strength of depth of field effect while the latter controls the time it takes for the focus point to transition from one child to an adjacent child.`,
     images: [
-      { original: "https://i.imgur.com/by5zndE.png", thumnail: "" },
-      { original: "https://i.imgur.com/o4cFNqy.png", thumnail: "" },
-      { original: "https://i.imgur.com/AVLO0Hc.png", thumnail: "" }
+      { original: "https://i.imgur.com/CzyYmg4.png", thumbnail: "" },
+      { original: "https://i.imgur.com/o4cFNqy.png", thumbnail: "" },
+      { original: "https://i.imgur.com/AVLO0Hc.png", thumbnail: "" }
     ],
     links: {
       github: "https://github.com/plexey/react-depth-of-field",
@@ -144,18 +150,18 @@ const content = {
         link: "https://github.com/plexey/react-depth-of-field"
       }
     ],
-    tags: ["javascript", "react"]
+    tags: ["javascript", "react", "husyky", "travis ci"]
   },
   reactreduxtodo: {
     name: "reactreduxtodo",
     heading: "React Redux Todo",
     subheading: "A todo app built with React & Redux",
     description:
-      "A simple todo app built with React & Redux. This app features animated list items using the react-flip-move package.",
+      "A simple todo app built with React & Redux. This app features animated list items using the react-flip-move package. This application adheres to the Ducks: Redux Reducer Bundles philosophy.",
     images: [
-      { original: "https://i.imgur.com/lTugTvJ.png", thumnail: "" },
-      { original: "https://i.imgur.com/oU4cqW3.png", thumnail: "" },
-      { original: "https://i.imgur.com/6cTh9mo.png", thumnail: "" }
+      { original: "https://i.imgur.com/lTugTvJ.png", thumbnail: "" },
+      { original: "https://i.imgur.com/oU4cqW3.png", thumbnail: "" },
+      { original: "https://i.imgur.com/6cTh9mo.png", thumbnail: "" }
     ],
     technologies: [
       {
@@ -207,10 +213,14 @@ const content = {
   personalwebsite: {
     name: "personalwebsite",
     heading: "Personal Website",
-    subheading: "My own personal website",
+    subheading:
+      "Built from the ground up in React and custom styled components.",
     description:
-      "This is the project write-up for the website you're using it right now! This website was built with React and features an extremely minimal webpack configuration. The application source code utilises styled-components, an implementation of CSS in JS, and thus contains no CSS files at all!",
-    images: [{ original: "https://i.imgur.com/liTvhh1.png", thumnail: "" }],
+      "This website functions as a portfolio of my recent web development projects. It features a simple UI built with contemporary web technologies in order to produce a consistent and fast user experience across different screen sizes and platforms.",
+    images: [
+      { original: "https://i.imgur.com/XVLTKAS.png" },
+      { original: "https://i.imgur.com/tBng7lG.png" }
+    ],
     technologies: [
       {
         name: "react",
@@ -250,13 +260,66 @@ const content = {
         link: "https://github.com/plexey/personal-website"
       }
     ],
-    tags: ["javascript", "react", "CSS in JS"]
+    tags: ["javascript", "react", "css in js", "firebase"]
   },
+
+  firebaseauthentication: {
+    name: "firebaseauthentication",
+    heading: "Firebase Authentication",
+    subheading:
+      "A React + Redux application showcasing Firebase user authentication and databasing",
+    description:
+      "A web app showcasing Firebase user authentication via the email/password sign-in method. This project functions as a highly extensible foundation for building out modern web apps. The application view layer is built with React, with all state management controlled via Redux and application routing controled via React Router. The application view layer was prototyped mostly via the CSS in JS solution: Styled Components.",
+    images: [
+      { original: "https://i.imgur.com/wOGstnV.png" },
+      { original: "https://i.imgur.com/sUoKdZN.png" }
+    ],
+    technologies: [
+      {
+        name: "firebase",
+        description: "a mobile and web application development platform ",
+        link: "https://firebase.google.com/"
+      },
+      {
+        name: "react",
+        description: "A JavaScript library for building user interfaces",
+        link: "https://reactjs.org/"
+      },
+      {
+        name: "redux",
+        description: "Predictable state container for JavaScript apps",
+        link: "https://github.com/reactjs/redux"
+      },
+      {
+        name: "react-router",
+        description: "Declarative routing for React",
+        link: "https://github.com/ReactTraining/react-router"
+      },
+      {
+        name: "styled-components",
+        description:
+          "Visual primitives for the component age. Use the best bits of ES6 and CSS to style your apps without stress",
+        link: "https://github.com/styled-components/styled-components"
+      }
+    ],
+    links: {
+      github: "https://github.com/plexey/firebase-authentication"
+    },
+    resources: [
+      {
+        name: "This project can be viewed on my",
+        alias: "GITHUB PAGE",
+        link: "https://github.com/plexey/firebase-authentication"
+      }
+    ],
+    tags: ["firebase", "react", "redux", "css in js"]
+  },
+
   reactstack: {
     name: "reactstack",
     heading: "React Stack",
     subheading:
-      "A React component facilitating stacking of and animated iteration through an arbitary set of elements",
+      "A React component facilitating stacking of and animated iteration through an set of elements",
     description:
       "Reack Stack is a react component that functions like an image gallery.",
     images: [
@@ -295,9 +358,7 @@ const content = {
     heading: "unary clock",
     subheading: "A simple unary clock web component",
     description: "A unary clock built with React and styled using CSS Grid",
-    images: [
-      { original: "https://i.imgur.com/MchaUbd.png" }
-    ],
+    images: [{ original: "https://i.imgur.com/MchaUbd.png" }],
     technologies: [
       {
         name: "react",
@@ -325,4 +386,4 @@ const content = {
   }
 };
 
-export default content;
+export default projects;
