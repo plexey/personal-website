ad9953cb4c8c7857986b05b30c7173ad7663a4e5
Thomas <tptrinca@gmail.com>
2018-03-01 00:03:14 +1100

    style: define min-height in order to fill vertical browser space

--- client/components/ContentWrapper.js
+++ client/components/ContentWrapper.js
@@ -5,7 +5,8 @@ const ContentWrapper = styled.div`
   grid-column-end: cline-3;
   font-family: ${props => props.theme.font};
   margin: 30px 0 30px 0;
-
+  min-height: 100vh;
+  
   @media screen and (max-width: 1000px) {
     ${ContentWrapper} {
       margin: 15px 0 15px 0;
