b313d75fdf42cc1fc99ce669888a16edb0fc4e7d
Thomas <tptrinca@gmail.com>
2018-03-01 00:10:37 +1100

    refactor: add ContentHeader component refactor:  define new Route conditionally rendering Home component

--- client/containers/Content.js
+++ client/containers/Content.js
@@ -2,17 +2,18 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import styled from "styled-components";
 import * as routes from "../constants/routes";
+import Home from "./Home";
 import About from "./About";
 import Projects from "./Projects";
 import Project from "./Project";
 import Contact from "./Contact";
+import ContentHeader from "./ContentHeader";
 
 const Wrapper = styled.div`
   display: grid;
-  grid-row-start: main3;
-  grid-row-end: main4;
+  grid-row-start: main2;
+  grid-row-end: main3;
   grid-template-columns: ${props => props.theme.deskGridTempCol};
-  min-height: 100vh;
   transition: 200ms ease all;
   background-color: #f4f4f4;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%23e0e0e0' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E");
@@ -34,6 +35,8 @@ class Content extends Component {
   render() {
     return (
       <Wrapper>
+        <ContentHeader />
+        <Route exact path={routes.HOME} component={Home} />
         <Route exact path={routes.ABOUT} component={About} />
         <Route exact path={routes.PROJECTS} component={Projects} />
         <Route exact path={routes.PROJECT} component={Project} />
