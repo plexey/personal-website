b747a6d0c6c9b547fcd1acd56b7535ae82793c1a
Thomas <tptrinca@gmail.com>
2018-03-01 00:02:26 +1100

    refactor: modify style for cross site style consistency

--- client/components/Button.js
+++ client/components/Button.js
@@ -1,23 +1,29 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  color: ${props => props.theme.textColor};
-  background: ${props => props.theme.brandColor};
-  padding: 10px 20px 10px 20px;
-  font-size: 19px;
-  font-family: ${props => props.theme.font};
   border: none;
-  border-radius: 3px;
-  margin: 0 10px 0 0;
+  font-family: ${props => props.theme.font};
+  background: ${props => props.theme.brandColor};
+  box-shadow: 0 3px 0 0 ${props => props.theme.brandColorDark};
+  margin: 0 10px 10px 0;
+  padding: 10px 20px 10px 20px;
+  font-size: 20px;
   font-weight: bold;
-  box-shadow: 0 4px 0 0 ${props => props.theme.brandColorDark},
-    0 7px 6px 0 hsla(0, 0%, 0%, 0.2);
-  transition: 200ms ease all;
   cursor: pointer;
+  color: ${props => props.theme.textColor};
+  transition: 200ms ease all;
 
-  ${Button}:active {
-    box-shadow: 0 1px 0 ${props => props.theme.brandColorDark};
-    transform: translateY(3px);
+  ${Button}:hover {
+    color: ${props => props.theme.brandColor};
+    background: ${props => props.theme.textColor};
+  }
+
+  @media screen and (max-width: 1000px) {
+    ${Button} {
+      font-size: 16px;
+      padding: 8px 15px 8px 15px;
+      margin: 0 5px 5px 0;
+    }
   }
 `;
 
