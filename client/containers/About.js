c76f651442773ace9f5c0b3f0b41dcf788d28855
Thomas <tptrinca@gmail.com>
2018-03-01 00:08:37 +1100

    refactor: remove deprecated styled components refactor: modify list item mapping to support array of objects refactor: remove portrait image

--- client/containers/About.js
+++ client/containers/About.js
@@ -14,44 +14,41 @@ const List = styled.ul``;
 const PortraitWrapper = styled.div`
   width: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-content: center;
+
+  @media screen and (max-width: 1000px) {
+    ${PortraitWrapper} {
+      justify-content: center;
+    }
+  }
 `;
 
 const Portrait = styled.img`
-  height: 250px;
-  width: 250px;
+  height: 220px;
+  width: 220px;
   border-radius: 3px;
   border-style: solid;
   border-color: hsl(0, 0%, 20%);
   border-width: 3px;
   margin: 20px 0 0 0;
+
+  @media screen and (max-width: 1000px) {
+    ${Portrait} {
+    }
+  }
 `;
 
-const Heading = styled.h2`
-  color: ${props => props.theme.textColor};
-  font-size: 25px;
-  font-weight: bold;
-  width: 100%;
-  text-align: center;
-`;
-
-const Subheading = styled.h3`
-  color: hsl(0, 0%, 35%);
-  font-size: 20px;
-  width: 100%;
-  text-align: center;
+const ListItemDescription = styled.em`
+  color: hsl(0, 0%, 40%);
+  line-height: 25px;
 `;
 
 const About = () => {
   return (
     <ContentWrapper>
-      <Heading>Tom Trinca - Web Developer & Designer</Heading>
-      <Subheading>Melbourne, Australia</Subheading>
-
-      <PortraitWrapper>
-        <Portrait src={about.image} alt="mugshot" />
-      </PortraitWrapper>
+      <SectionHeading>Web Developer & Designer</SectionHeading>
+      <SectionSubheading>Melbourne, Australia</SectionSubheading>
 
       <Section>
         <SectionContent>
@@ -65,7 +62,17 @@ const About = () => {
           <SectionSubheading>{skill.subheading}</SectionSubheading>
           <SectionContent>
             <List>
-              {skill.items.map(item => <ListItem key={item}>{item}</ListItem>)}
+              {skill.items.map(
+                item =>
+                  typeof item === "string" ? (
+                    <ListItem key={`${item}-${index}`}>{item}</ListItem>
+                  ) : (
+                    <ListItem key={`${item.name}-${index}`}>
+                      {item.name} -{" "}
+                      <ListItemDescription>{item.info}</ListItemDescription>
+                    </ListItem>
+                  )
+              )}
             </List>
           </SectionContent>
         </Section>
